122318f7e025abfc693b4906ac35d84468212ae2
loicngr <pro.nogierloic@gmail.com>
2021-01-24 12:34:51 +0100

    Add sign up router

--- lib/src/views/router.dart
+++ lib/src/views/router.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:planifago/src/views/ui/landing.dart';
 import 'package:planifago/src/views/ui/login.dart';
+import 'package:planifago/src/views/ui/signup.dart';
 
 Route landingRoute() {
   return PageRouteBuilder(
@@ -14,6 +15,15 @@ Route landingRoute() {
 Route landingSignInRoute({List<dynamic> users}) {
   return PageRouteBuilder(
     pageBuilder: (context, animation, secondaryAnimation) => Login(),
+    transitionsBuilder: (context, animation, secondaryAnimation, child) {
+      return child;
+    },
+  );
+}
+
+Route landingSignUpRoute({List<dynamic> users}) {
+  return PageRouteBuilder(
+    pageBuilder: (context, animation, secondaryAnimation) => SignUp(),
     transitionsBuilder: (context, animation, secondaryAnimation, child) {
       return child;
     },
