d9576ed215311ed14c6770ee2f08d608f7e1212b
loicngr <pro.nogierloic@gmail.com>
2021-01-24 12:39:42 +0100

    Update login form fields

--- lib/src/views/ui/login.dart
+++ lib/src/views/ui/login.dart
@@ -6,6 +6,82 @@ import 'package:planifago/src/views/router.dart';
 
 class Login extends StatelessWidget {
   final _formKey = GlobalKey<FormState>();
+  final _passwordController = TextEditingController();
+
+  Widget _buildEmail() {
+    return TextFormField(
+      keyboardType: TextInputType.emailAddress,
+      validator: (value) => !isEmail(value) ? "Sorry, we do not recognize this email address" : null,
+      style: TextStyle(color: Color(ConstantColors.black)),
+      decoration: buildInputDecoration("Email", 'assets/images/email.png'),
+    );
+  }
+  Widget _buildPassword() {
+    return TextFormField(
+      obscureText: true,
+      autocorrect: false,
+      enableSuggestions: false,
+      keyboardType: TextInputType.text,
+      controller: _passwordController,
+      validator: (value) =>
+      value.length <= 6 ? "Password must be 6 or more characters in length" : null,
+      style: TextStyle(color: Color(ConstantColors.black)),
+      decoration:
+      buildInputDecoration("Password", 'assets/images/password.png'),
+    );
+  }
+  Widget _buildSignUpButton(BuildContext context) {
+    return Container(
+      height: (deviceHeight(context) / 3) - ConstantSize.landingLoginButtonHeight,
+      width: deviceWidth(context),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Material(
+                elevation: 0.0,
+                borderRadius: BorderRadius.circular(10.0),
+                color: Color(ConstantColors.blue),
+                child: MaterialButton(
+                  minWidth: ConstantSize.landingButtonWidth,
+                  height: ConstantSize.landingButtonHeight,
+                  padding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+                  onPressed: () {
+                    _validateAndSubmit();
+                  },
+                  child: Text("Log In",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(color: Color(ConstantColors.white), fontWeight: FontWeight.bold, fontSize: 20.00)),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 16.0),
+                child: RichText(
+                  text: TextSpan(
+                    text: 'Back',
+                    style: TextStyle(fontSize: 13.00, color: Color(ConstantColors.blue), fontWeight: FontWeight.bold),
+                    recognizer: TapGestureRecognizer()
+                      ..onTap = () => {
+                        Navigator.of(context).push(landingRoute())
+                      },
+                  ),
+                ),
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+
+  _validateAndSubmit() {
+    if (_formKey.currentState.validate()) {
+      print('validate form');
+      // _formKey.currentState.reset();
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -44,36 +120,8 @@ class Login extends StatelessWidget {
                               width: ConstantSize.landingButtonWidth,
                               child: Column(
                                 children: [
-                                  TextFormField(
-                                    keyboardType: TextInputType.emailAddress,
-                                    decoration: const InputDecoration(
-                                      hintText: 'Email',
-                                    ),
-                                    validator: (value) {
-                                      if (value.isEmpty) {
-                                        return 'Please enter your email address';
-                                      }
-                                      else if (!isEmail(value)) {
-                                        return 'Please enter a valid email address';
-                                      }
-                                      return null;
-                                    },
-                                  ),
-                                  TextFormField(
-                                    obscureText: true,
-                                    autocorrect: false,
-                                    enableSuggestions: false,
-                                    keyboardType: TextInputType.text,
-                                    decoration: const InputDecoration(
-                                      hintText: 'Password',
-                                    ),
-                                    validator: (value) {
-                                      if (value.isEmpty) {
-                                        return 'Please enter your password';
-                                      }
-                                      return null;
-                                    },
-                                  )
+                                  _buildEmail(),
+                                  _buildPassword()
                                 ],
                               ),
                             ),
@@ -83,52 +131,7 @@ class Login extends StatelessWidget {
                     ],
                   ),
                 ),
-                Container(
-                  height: (deviceHeight(context) / 3) - ConstantSize.landingLoginButtonHeight,
-                  width: deviceWidth(context),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Material(
-                            elevation: 0.0,
-                            borderRadius: BorderRadius.circular(10.0),
-                            color: Color(ConstantColors.blue),
-                            child: MaterialButton(
-                              minWidth: ConstantSize.landingButtonWidth,
-                              height: ConstantSize.landingButtonHeight,
-                              padding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
-                              onPressed: () {
-                                if (_formKey.currentState.validate()) {
-                                  print('log in !!');
-                                  // _formKey.currentState.reset();
-                                }
-                              },
-                              child: Text("Log In",
-                                  textAlign: TextAlign.center,
-                                  style: TextStyle(color: Color(ConstantColors.white), fontWeight: FontWeight.bold, fontSize: 20.00)),
-                            ),
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.only(top: 16.0),
-                            child: RichText(
-                              text: TextSpan(
-                                text: 'Back',
-                                style: TextStyle(fontSize: 13.00, color: Color(ConstantColors.blue), fontWeight: FontWeight.bold),
-                                recognizer: TapGestureRecognizer()
-                                  ..onTap = () => {
-                                    Navigator.of(context).push(landingRoute())
-                                  },
-                              ),
-                            ),
-                          )
-                        ],
-                      )
-                    ],
-                  ),
-                )
+                _buildSignUpButton(context)
               ],
             ),
           ),
