bdbee10fff348164b1452168f7786e5f4af93f14
loicngr <pro.nogierloic@gmail.com>
2021-01-28 13:20:08 +0100

    Add sign up route

--- lib/router.dart
+++ lib/router.dart
@@ -1,11 +1,11 @@
+/// Packages
 import 'package:flutter/material.dart';
 
-/**
- * Screens
- */
+/// Screens
 import 'package:planifago/screens/home/home_page.dart';
 import 'package:planifago/screens/landing/landing_page.dart';
 import 'package:planifago/screens/landing/login_page.dart';
+import 'package:planifago/screens/landing/signup_page.dart';
 
 Route landingRoute() {
   return PageRouteBuilder(
@@ -25,11 +25,29 @@ Route landingRoute() {
   );
 }
 
-Route landingSignInRoute() {
+Route landingLogInRoute() {
   return PageRouteBuilder(
     pageBuilder: (context, animation, secondaryAnimation) => LoginPage(),
     transitionsBuilder: (context, animation, secondaryAnimation, child) {
       var begin = Offset(1, 0);
+      var end = Offset.zero;
+      var curve = Curves.ease;
+
+      var tween = Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
+
+      return SlideTransition(
+        position: animation.drive(tween),
+        child: child,
+      );
+    },
+  );
+}
+
+Route landingSignUpRoute() {
+  return PageRouteBuilder(
+    pageBuilder: (context, animation, secondaryAnimation) => SignUpPage(),
+    transitionsBuilder: (context, animation, secondaryAnimation, child) {
+      var begin = Offset(0, 1);
       var end = Offset.zero;
       var curve = Curves.ease;
 
