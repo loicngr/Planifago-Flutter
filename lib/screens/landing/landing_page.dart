b23c42618f498aafbce25c421208d08c67d59f10
loicngr <pro.nogierloic@gmail.com>
2021-02-11 10:43:15 +0100

    Add language selector

--- lib/screens/landing/landing_page.dart
+++ lib/screens/landing/landing_page.dart
@@ -2,12 +2,12 @@
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:planifago/language.dart';
+import 'package:provider/provider.dart';
 
 /// Utils
 import 'package:planifago/utils/utils.dart';
 import 'package:planifago/utils/constants.dart';
-
-/// Router
 
 class LandingPage extends StatefulWidget {
   @override
@@ -15,17 +15,76 @@ class LandingPage extends StatefulWidget {
 }
 
 class _LandingPageState extends State<LandingPage> {
+  AppLanguage appLanguage;
+  String languageValue = '';
+  Map<String, String> languages = {};
+
+  Future<Widget> _buildlanguage() async {
+    await appLanguage.fetchLocale();
+
+    languages = LanguageUtils.getLanguages(context);
+
+    setState(() {
+      languageValue = LanguageUtils.getKeyValue(
+          languages, appLanguage.appLocal.languageCode);
+    });
+
+    return DropdownButton(
+      value: languageValue,
+      icon: null,
+      iconSize: 24,
+      elevation: 16,
+      style: TextStyle(color: Color(ConstantColors.black)),
+      underline: null,
+      onChanged: (String newValue) async {
+        String value = LanguageUtils.getValueKey(languages, newValue);
+        setState(() {
+          languageValue = value;
+        });
+        appLanguage.changeLanguage(Locale(value));
+      },
+      items: languages.values.map<DropdownMenuItem<String>>((String value) {
+        return DropdownMenuItem<String>(
+          value: value,
+          child: Text(value),
+        );
+      }).toList(),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    /// If user is connected to app
-    /// Redirect to Home page and defined Home page as root
-    // redirectIfNotConnected(context);
+    appLanguage = Provider.of<AppLanguage>(context);
 
     return Scaffold(
       body: Center(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
+            Container(
+              height: landingLanguageHeight(context),
+              width: deviceWidth(context),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  Padding(
+                      padding: EdgeInsets.fromLTRB(0, 0, 20, 0),
+                      child: FutureBuilder(
+                          future: _buildlanguage(),
+                          builder: (context, snapshot) {
+                            if (snapshot.connectionState ==
+                                        ConnectionState.none &&
+                                    snapshot.hasData == null ||
+                                snapshot.data == null) {
+                              return getLoader;
+                            }
+
+                            return snapshot.data;
+                          })),
+                ],
+              ),
+            ),
             Container(
               height: landingLogoBlocHeight(context),
               width: deviceWidth(context),
